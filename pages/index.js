78882b1bfa7238b9c12a7dc55f0a0ebbd353df45
anum <hello@anum.io>
2022-08-23 00:23:58 +0200

    Commented out main content

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
@@ -52,7 +52,7 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
 
       <footer className={styles.footer}>
