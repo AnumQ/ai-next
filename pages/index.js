afd45a378c85473a4436db804c0c0ebd73b98580
anum <hello@anum.io>
2022-09-05 00:52:17 +0200

    refactor github logo

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.css";
 import Script from "next/script";
 import { LinkedInLogo } from "../components/SVGs/LinkedInLogo";
 import { TwitterLogo } from "../components/SVGs/TwitterLogo";
+import { GithubLogo } from "../components/SVGs/GithubLogo";
 import { useEffect } from "react";
 
 export default function Home() {
@@ -202,31 +203,26 @@ export default function Home() {
                         <li>
                           <a href="https://twitter.com/anumqudsia">
                             <TwitterLogo
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
+                              width="26"
+                              height="26"
+                              viewBox="0 0 22 22"
                             />
                             <span className="screen-reader-text">Twitter</span>
                           </a>
                           <a href="https://www.linkedin.com/in/anum-qudsia-75199262/">
                             <LinkedInLogo
-                              width="48"
-                              height="48"
-                              viewBox="0 0 32 32"
+                              width="24"
+                              height="24"
+                              viewBox="0 0 16 16"
                             />
                             <span className="screen-reader-text">LinkedIn</span>
                           </a>
                           <a href="https://github.com/AnumQ/">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width={36}
-                              height={48}
-                              fill="currentColor"
-                              className="bi bi-github"
-                              viewBox="0 -1 24 36"
-                            >
-                              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
-                            </svg>
+                            <GithubLogo
+                              width="24"
+                              height="24"
+                              viewBox="0 0 16 16"
+                            />
                           </a>
                           <span className="screen-reader-text">Github</span>
                         </li>
