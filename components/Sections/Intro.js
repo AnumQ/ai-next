fe4bd9b1e1358bc2435d55c42749a13333f022e0
anum <hello@anum.io>
2022-10-01 10:04:43 +0200

    Separate ArrowSVG to its own file

--- components/Sections/Intro.js
+++ components/Sections/Intro.js
@@ -1,26 +1,9 @@
 import useData from "components/hooks/useData";
+import ArrowSVG from "components/SVGs/ArrowSVG";
 import React from "react";
 
 export const Intro = () => {
   const { introData: data, constants } = useData();
-
-  const ArrowSVG = () => {
-    return (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={24}
-        height={24}
-        viewBox="0 0 24 24"
-        style={{
-          fill: "rgba(0, 0, 0, 1)",
-          transform: "rotate(180deg)",
-          msFilter: "progid:DXImageTransform.Microsoft.BasicImage(rotation=2)",
-        }}
-      >
-        <path d="M21 11L6.414 11 11.707 5.707 10.293 4.293 2.586 12 10.293 19.707 11.707 18.293 6.414 13 21 13z" />
-      </svg>
-    );
-  };
 
   return (
     <section id="intro" className="s-intro target-section">
