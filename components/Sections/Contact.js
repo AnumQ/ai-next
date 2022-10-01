579e252afc7311da8f17c7088ab908a49812043c
anum <hello@anum.io>
2022-10-01 10:46:37 +0200

    Add strings from Contact.js to constants

--- components/Sections/Contact.js
+++ components/Sections/Contact.js
@@ -5,6 +5,13 @@ import {
   GITHUB_URL,
   EMAIL,
   MOBILE,
+  COPYRIGHT,
+  COMPANY_VALUE,
+  VAT,
+  CONTACT_PRE_TITLE,
+  CONTACT_TITLE,
+  EMAIL_TITLE,
+  COMPANY_TITLE,
 } from "components/constants";
 import GithubLink from "components/Links/GithubLink";
 import LinkedInLink from "components/Links/LinkedInLink";
@@ -15,21 +22,15 @@ export default function Contact() {
     <section id="contact" className="s-contact target-section">
       <div className="row section-header section-header--dark">
         <div className="column lg-12">
-          <h3 className="text-pretitle">Get In Touch</h3>
-          <h1 className="text-display-title">
-            Are you in need of a developer? Let’s work together.
-          </h1>
+          <h3 className="text-pretitle">{CONTACT_PRE_TITLE}</h3>
+          <h1 className="text-display-title">{CONTACT_TITLE}</h1>
         </div>
       </div>
       <div className="row row-x-center text-center s-contact__content">
         <div className="column lg-12">
           <p className="lead"></p>
           <div className="btn-wrap">
-            <a
-              href={`mailto:${EMAIL}`}
-              // href="mailto:hello@anum.io"
-              className="btn btn--primary"
-            >
+            <a href={`mailto:${EMAIL}`} className="btn btn--primary">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width={24}
@@ -43,7 +44,7 @@ export default function Contact() {
               >
                 <path d="M20,4H4C2.897,4,2,4.897,2,6v12c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2V6C22,4.897,21.103,4,20,4z M20,6v0.511 l-8,6.223L4,6.512V6H20z M4,18V9.044l7.386,5.745C11.566,14.93,11.783,15,12,15s0.434-0.07,0.614-0.211L20,9.044L20.002,18H4z" />
               </svg>
-              Write An Email
+              {EMAIL_TITLE}
             </a>
           </div>
         </div>
@@ -80,25 +81,25 @@ export default function Contact() {
             <h4 className="h6">Contact Me</h4>
             <ul className="contact-list">
               <li>
-                <a href={`mailto:${EMAIL}`}>hello@anum.io</a>
+                <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
               </li>
               <li>
-                <a href={`tel:${MOBILE}`}>+47 932 86 096</a>
+                <a href={`tel:${MOBILE}`}>{MOBILE}</a>
               </li>
             </ul>
           </div>
           <div className="column lg-3 md-12 s-footer__block contact-number">
-            <h4 className="h6">Company</h4>
+            <h4 className="h6">{COMPANY_TITLE}</h4>
             <ul className="contact-list">
-              <li>FIRETECH AS</li>
-              <li>VAT: 917 876 770</li>
+              <li>{COMPANY_VALUE}</li>
+              <li>{VAT}</li>
             </ul>
           </div>
         </div>{" "}
         {/* end s-footer__top */}
         <div className="row s-footer__bottom">
           <div className="column ss-copyright">
-            <span>© Copyright Anum Codes 2022</span>
+            <span>{COPYRIGHT}</span>
             <span>
               {/* Design by <a href="https://www.styleshout.com/">StyleShout</a> */}
             </span>
--- components/constants.js
+++ components/constants.js
@@ -7,3 +7,11 @@ export const MOBILE = "4793286096";
 export const SITE_TITLE = "Anum Codes | Software Engineer";
 export const SITE_META_DESCRIPTION =
   "frontend developer react developer swift developer mobile developer senior frontend utvikler mobil utvikler developer utvikler react nextjs typescript javascript utvikler";
+export const COPYRIGHT = "© Copyright Anum Codes 2022";
+export const COMPANY_TITLE = "COMPANY";
+export const COMPANY_VALUE = "FIRETECH AS";
+export const VAT = "VAT: 917 876 770";
+export const CONTACT_PRE_TITLE = "Get In Touch";
+export const CONTACT_TITLE =
+  "Are you in need of a developer? Let’s work together.";
+export const EMAIL_TITLE = "Write An Email";
