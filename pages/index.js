55f2e9989c7c3ad0353155c340ec05635c648e81
anum <hello@anum.io>
2022-10-01 10:35:10 +0200

    Cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { useEffect } from "react";
 import React from "react";
 import Header from "components/Header/Header";
 import Intro from "components/Sections/Intro";
