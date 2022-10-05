c665567cd909b581d0a5b3dbd57c0816b51c5f81
anum <hello@anum.io>
2022-10-05 09:15:26 +0200

    Add keyframe animation headerAnimate to header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -8,54 +8,57 @@ import TwitterLink from "components/Links/TwitterLink";
 export default function Header() {
   return (
     <>
-      <AnimatePresence>
+      {/* <AnimatePresence>
         <motion.header
           className="s-header"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-        >
-          <div className="s-header__block">
-            <div className="s-header__logo">
-              <a className="logo" href="">
-                <Image
-                  src="/images/logo.svg"
-                  alt="Homeparge"
-                  width="104"
-                  height="78"
-                />
-              </a>
-            </div>
-            {/* <a className="s-header__menu-toggle">
+          initial={{ top: "-6rem" }}
+          animate={{ top: 0 }}
+          transition={{ ease: "easeOut", duration: 0.5 }}
+        > */}
+      <header className="s-header">
+        <div className="s-header__block">
+          <div className="s-header__logo">
+            <a className="logo" href="">
+              <Image
+                src="/images/logo.svg"
+                alt="Homeparge"
+                width="104"
+                height="78"
+              />
+            </a>
+          </div>
+          {/* <a className="s-header__menu-toggle">
           <span>Menu</span>
         </a> */}
-          </div>
-          {/* end s-header__block */}
-          <div className="row s-header__nav-wrap">
-            <nav className="s-header__nav">
-              <ul>
-                <li className="current">
-                  <a href="#intro" className="smoothscroll">
-                    Intro
-                  </a>
-                </li>
-                {/* <li><a href="#services" class="smoothscroll">What We Do</a></li> */}
-                {/* <li><a href="#works" class="smoothscroll">My projects</a></li> */}
-                <li>
-                  <a href="#contact" className="smoothscroll">
-                    Get In Touch
-                  </a>
-                </li>
-              </ul>
-            </nav>
-            <ul className="s-header__social">
-              <TwitterLink />
-              <LinkedInLink />
-              <GithubLink />
+        </div>
+        {/* end s-header__block */}
+        <div className="row s-header__nav-wrap">
+          <nav className="s-header__nav">
+            <ul>
+              <li className="current">
+                <a href="#intro" className="smoothscroll">
+                  Intro
+                </a>
+              </li>
+              {/* <li><a href="#services" class="smoothscroll">What We Do</a></li> */}
+              {/* <li><a href="#works" class="smoothscroll">My projects</a></li> */}
+              <li>
+                <a href="#contact" className="smoothscroll">
+                  Get In Touch
+                </a>
+              </li>
             </ul>
-          </div>
-          {/* end s-header__nav-wrap */}
-        </motion.header>
-      </AnimatePresence>
+          </nav>
+          <ul className="s-header__social">
+            <TwitterLink />
+            <LinkedInLink />
+            <GithubLink />
+          </ul>
+        </div>
+        {/* end s-header__nav-wrap */}
+        {/* </motion.header> */}
+        {/* </AnimatePresence> */}
+      </header>
     </>
   );
 }
