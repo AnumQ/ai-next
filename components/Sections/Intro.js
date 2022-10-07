d526e15968b5e4f72638472f448843d5f5a5c927
anum <hello@anum.io>
2022-10-07 14:26:23 +0200

    Add animation to intro-text, intro-section and intro-background

--- components/Sections/Intro.js
+++ components/Sections/Intro.js
@@ -2,41 +2,61 @@ import useData from "components/hooks/useData";
 import ArrowSVG from "components/SVGs/ArrowSVG";
 import React from "react";
 import s from "./Intro.module.css";
+import { motion, useScroll, useSpring, AnimatePresence } from "framer-motion";
 
 export default function Intro() {
   const { introData: data, constants } = useData();
 
   return (
-    // <section id="intro" className="s_intro target-section">
     <section id="intro" className={`${s.s_intro}`}>
       <div className={s.row}>
-        <div className={s.s_intro_background}></div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 0.3,
+          }}
+          transition={{ ease: "easeIn", duration: 1, delay: 1.5 }}
+          className={s.s_intro_background}
+        ></motion.div>
         <div className={s.column}>
-          <div className={s.s_intro__top_block}>
-            {/* <span className="s_intro__bg" /> */}
-            <h1 className={s.s_intro__text}>
-              {data.titleIntro1} <br />
-              <span>{data.titleIntro2}</span>
-            </h1>
-            {/* <a href="#contact" className="s_intro__scroll_down smoothscroll"> */}
-            <a href="#contact" className={s.s_intro__scroll_down}>
-              <span>{constants.scrollDown}</span>
-              <ArrowSVG />
-            </a>
-          </div>
+          <AnimatePresence>
+            <motion.div
+              className={s.s_intro__top_block}
+              initial={{ top: 40 }}
+              animate={{
+                top: 0,
+                opacity: 1,
+              }}
+              transition={{ ease: "easeIn", duration: 0.5, delay: 0.35 }}
+            >
+              <h1 className={s.s_intro__text}>
+                {data.titleIntro1} <br />
+                <span>{data.titleIntro2}</span>
+              </h1>
+              <a href="#contact" className={s.s_intro__scroll_down}>
+                <span>{constants.scrollDown}</span>
+                <ArrowSVG />
+              </a>
+            </motion.div>
+          </AnimatePresence>
         </div>
       </div>
-      {/* <div className="row row_x_center s_intro__about_wrap"> */}
       <div className={`${s.row} ${s.row_x_center}`}>
-        {/* <div className="column s_intro__about"> */}
-        <div className={`${s.column} ${s.s_intro__about}`}>
-          <h2 className={s.s_intro__about_title}>{data.titleAbout}</h2>
-          <p>
-            {data.titleDescription1}
-            <b>{data.titleDescription2}</b>
-          </p>
-          <p>{data.titleDescription3}</p>
-        </div>
+        <AnimatePresence>
+          <motion.div
+            initial={{ top: 40, opacity: 0 }}
+            animate={{ top: 0, opacity: 1 }}
+            transition={{ ease: "easeIn", duration: 0.6, delay: 0.85 }}
+            className={`${s.column} ${s.s_intro__about}`}
+          >
+            <h2 className={s.s_intro__about_title}>{data.titleAbout}</h2>
+            <p>
+              {data.titleDescription1}
+              <b>{data.titleDescription2}</b>
+            </p>
+            <p>{data.titleDescription3}</p>
+          </motion.div>
+        </AnimatePresence>
       </div>
     </section>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           rel="preload"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@500;700&family=Manrope&display=swap"
