453457fc4a5c782dbbf3f99c9936b101de08a6a9
Anum Qudsia <AnumQ@users.noreply.github.com>
2023-10-02 22:59:44 +0200

    fix errors

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
 export default function CV() {
-  async function getData() {
-    const result = await fetch("file.json");
+  // async function getData() {
+  //   const result = await fetch("file.json");
 
-    if (!result.ok) {
-      return console.error("Error reading file.json");
-    }
+  //   if (!result.ok) {
+  //     return console.error("Error reading file.json");
+  //   }
 
-    const fileData = await result.json();
+  //   const fileData = await result.json();
 
-    console.log(fileData);
-  }
+  //   console.log(fileData);
+  // }
 
-  getData();
+  // getData();
   return <>Change this</>;
 }
