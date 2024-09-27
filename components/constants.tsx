1821e8060ee9162a99f8bebd8a084c34cf0e1649
Anum Qudsia <AnumQ@users.noreply.github.com>
2024-09-27 06:10:11 +0200

    Update contact title

--- components/constants.tsx
+++ components/constants.tsx
@@ -13,5 +13,5 @@ export const COMPANY_VALUE = "FIRETECH AS";
 export const VAT = "VAT: 917 876 770";
 export const CONTACT_PRE_TITLE = "Get In Touch";
 export const CONTACT_TITLE =
-  "Do you need help in automating processes using technology? Let’s work together.";
+  "Are you in need of a developer? Let’s work together.";
 export const EMAIL_TITLE = "Write to me";
